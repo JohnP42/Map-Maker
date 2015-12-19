34c4b5e39d400e09d00a6823dc87eb4081214ca0
John <johnpolhill42@gmail.com>
2015-12-19 17:16:31 +0000

    add function to generate code for map

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ $(document).ready(function(){
 		$(this).attr("class", $("#select-tile").val());
 	});
 
+	$("#generate").on("click", function(event) {
+		$("#code").text(mapToCode());
+	});
+
 });
 
 var newMap = function(width, height){
@@ -24,4 +28,33 @@ var newMap = function(width, height){
 		}
 		$("#map").append(row);
 	}
+}
+
+var mapToCode = function(){
+	var code = "[\n"
+
+	$("#map tr").each(function() {
+		code += "["
+		$(this).children("td").each(function() {
+			if($(this).attr("class") === "tile0")
+				code += "new Plain(), "
+			if($(this).attr("class") === "tile1")
+				code += "new Forest(), "
+			if($(this).attr("class") === "tile2")
+				code += "new Mountain(), "
+			if($(this).attr("class") === "tile3")
+				code += "new Water(), "
+			if($(this).attr("class") === "tile4")
+				code += "new HQ(), "
+			if($(this).attr("class") === "tile5")
+				code += "new City(), "
+			if($(this).attr("class") === "tile6")
+				code += "new Factory(), "
+		});
+		code = code.substring(0, code.length - 1);
+		code += "],\n"
+	});
+
+	code += "];"
+	return code;
 }
